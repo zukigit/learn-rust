1f6bf6367fc4f98640fc56524dbd4173ba3d8d20
zuki@rocky10 <zukidono@gmail.com>
2026-02-03 23:46:48 +0700

    added formatted_print

--- src/modules/formatted_print.rs
+++ src/modules/formatted_print.rs
@@ -1,3 +1,12 @@
 pub fn formatted_print() {
+    println!("{} days", 10);
+    println!("{0} lives in {1}. {2} lives in {1} too", "zuki", "Rangoon", "wai");
 
+    println!("{sub} {verb} {obj}",
+        sub="he",
+        verb="is",
+        obj="good");
+    
+    println!("{number:0>width$}", number=1, width=5);
+    println!("{number:0>width$}", number=1, width=5);
 }
