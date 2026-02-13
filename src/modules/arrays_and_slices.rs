3c8ed427ba4df9c9f706695aa07999a57a3b1b04
zuki@rocky10 <zukidono@gmail.com>
2026-02-13 12:49:40 +0700

    adding vect

--- src/modules/arrays_and_slices.rs
+++ src/modules/arrays_and_slices.rs
@@ -14,4 +14,11 @@ pub fn arrays_and_slices() {
     let empty_array: [u32; 0] = [];
     assert_eq!(&empty_array, &[]);
     assert_eq!(&empty_array, &[][..]); // Same but more verbose
+
+    let mut vet: Vec<&str> = Vec::new();
+
+    vet.push("10");
+    vet.push("hello");
+
+    assert_eq!(vet.capacity(), 0);
 }
